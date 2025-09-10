7672e41e2e15f6f668766512cd0c60cf9de95b17
MrMagoo8888 <amwilson218@gmail.com>
2025-09-10 19:23:58 +0100

    Bug fixin for fat 1

--- src/bootloader/stage2/stdint.h
+++ src/bootloader/stage2/stdint.h
@@ -11,7 +11,12 @@ typedef unsigned long long int uint64_t;
 
 typedef uint8_t bool;
 
-#define false 0
-#define true 1
+#define NULL        ((void*)0)
+#define min(a,b)    ((a) < (b) ? (a) : (b))
+#define max(a,b)    ((a) > (b) ? (a) : (b))
 
-#define NULL ((void*)0)
+// may have unwanted and unintended consequences cause of a and b being evaluated multiple times; plz fix later me. THX!!!
+
+// also probs move to a more general header file later
+
+// byeee  19:22 10/9/2025
