21738e9f3366fb3e249a725b15d377f27d4b58a0
MrMagoo8888 <amwilson218@gmail.com>
2025-10-25 00:10:51 +0100

    Giant improvement

--- src/kernel/arch/i686/keyboard.h
+++ src/kernel/arch/i686/keyboard.h
@@ -2,7 +2,7 @@
 
 // Ensure SCROLLBACK_LINES and SCREEN_WIDTH are defined as macros before this line
 #ifndef SCROLLBACK_LINES
-#define SCROLLBACK_LINES 100 // or another suitable constant value
+#define SCROLLBACK_LINES 2 // or another suitable constant value
 #endif
 
 #ifndef SCREEN_WIDTH
@@ -16,3 +16,6 @@ extern int scrollback_view;
 
 void i686_Keyboard_Initialize();
 void keyboard_irq_handler(Registers* regs);
+
+// Reads a line of input from the keyboard into the provided buffer.
+void gets(char* buffer, int size);
