335f267a17baeddf8b6e61930df5d090f3290d44
MrMagoo8888 <amwilson218@gmail.com>
2025-10-25 17:26:37 +0100

    IT READS

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 # The first line is commented out as it causes a "write lock" error.
 # We will use the second line, which boots from a floppy and uses a separate hard disk image.
-qemu-system-i386 -drive file=build/main_floppy.img,format=raw,if=floppy -drive file=build/main_hdd.img,format=raw,if=ide,index=0,media=disk
+qemu-system-i386 -boot order=a -drive file=build/main_floppy.img,format=raw,if=floppy -drive file=build/main_hdd.img,format=raw,if=ide,index=0,media=disk
