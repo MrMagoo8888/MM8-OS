e3d32794e5f4f2b7b4b1f279593b7c9cb98851ac
MrMagoo8888 <amwilson218@gmail.com>
2025-10-31 21:21:41 +0000

    QoL

---
+++ src/kernel/commands/credits.c
@@ -0,0 +1,50 @@
+#include "stdio.h"
+#include "string.h"
+
+void credits_followers() {
+    printf("Credits to our Wonderful Followers:\n\n");
+    printf(" - MrJBMG\n");
+    printf(" - Una\n");
+    printf(" - ChrisWestbro\n");
+    printf(" - Bobby\n");
+    printf(" - Alex\n\n");
+
+    printf(" - TheSapphireStarship\n");
+    printf(" - RhiannonJourney\n");
+
+    
+}
+
+void credits_streamers() {
+
+    printf("\n\nCredits to my Supportive Streamers:\n\n");
+    printf(" - DIGITALILIAD\n");
+    
+
+
+}
+
+void credits_me() {
+
+    printf("\n\n\nSpecial Thanks to Myself for making this OS possible!\n\n");
+    printf(" - MM8\n");
+    printf("   - Founder of MM8-OS\n");
+    printf("   - Otherwise Known As MrMagoo8888\n");
+    printf("   - Creator, Developer, Tester, Streamer\n");
+
+
+}
+
+
+
+
+void credits() {
+
+    clrscr();
+    printf("MM8-OS Credits\n\n");
+
+    credits_followers();
+    credits_streamers();
+    credits_me();
+    
+}
