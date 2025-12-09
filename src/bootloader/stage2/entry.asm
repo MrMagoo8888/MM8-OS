7a5d9ea7c92db3f0ad19b73ae8ae88f8a1a74b51
MrMagoo8888 <amwilson218@gmail.com>
2025-12-09 18:22:58 +0000

    NowWereGettingSomeplace

--- src/bootloader/stage2/entry.asm
+++ src/bootloader/stage2/entry.asm
@@ -6,6 +6,7 @@ extern __bss_start
 extern __end
 
 extern start
+global vbe_screen
 global entry
 
 entry:
@@ -19,6 +20,13 @@ entry:
     mov ss, ax
     mov sp, 0xFFF0
     mov bp, sp
+
+    ; set VBE mode 1920x1080x32
+    mov ax, 1920
+    mov bx, 1080
+    mov cl, 32
+    call vbe_set_mode
+    ; carry flag will be set on error
 
     ; switch to protected mode
     call EnableA20          ; 2 - Enable A20 gate
@@ -117,6 +125,104 @@ LoadGDT:
     lgdt [g_GDTDesc]
     ret
 
+; vbe_set_mode: Sets a VESA mode
+; In:   AX = Width, BX = Height, CL = Bits per pixel
+; Out:  FLAGS = Carry clear on success, set on error.
+;       vbe_screen structure is filled on success.
+vbe_set_mode:
+    [bits 16]
+    mov [.width], ax
+    mov [.height], bx
+    mov [.bpp], cl
+
+    sti
+
+    push es
+    mov ax, 0x4F00              ; get VBE BIOS info
+    mov di, vbe_info_block
+    int 0x10
+    pop es
+
+    cmp ax, 0x4F                ; BIOS doesn't support VBE?
+    jne .error
+
+    mov ax, word [vbe_info_block.video_modes_ptr]
+    mov [.offset], ax
+    mov ax, word [vbe_info_block.video_modes_ptr+2]
+    mov [.segment], ax
+
+    mov ax, [.segment]
+    mov fs, ax
+    mov si, [.offset]
+
+.find_mode:
+    mov dx, [fs:si]
+    add si, 2
+    mov [.offset], si
+    mov [.mode], dx
+
+    cmp word [.mode], 0xFFFF    ; end of list?
+    je .error
+
+    push es
+    mov ax, 0x4F01              ; get VBE mode info
+    mov cx, [.mode]
+    mov di, mode_info_block
+    int 0x10
+    pop es
+
+    cmp ax, 0x4F
+    jne .next_mode ; Some BIOSes return failure for unsupported modes, so just try next
+
+    ; Check if mode attributes match. We need linear framebuffer support.
+    mov ax, [mode_info_block.attributes]
+    test ax, (1 << 7) ; Is LFB available?
+    jz .next_mode
+
+    mov ax, [.width]
+    cmp ax, [mode_info_block.width]
+    jne .next_mode
+
+    mov ax, [.height]
+    cmp ax, [mode_info_block.height]
+    jne .next_mode
+
+    mov al, [.bpp]
+    cmp al, [mode_info_block.bpp]
+    jne .next_mode
+
+    ; Found the mode, set it
+    mov ax, 0x4F02
+    mov bx, [.mode]
+    or bx, 0x4000               ; enable Linear Frame Buffer
+    int 0x10
+
+    cmp ax, 0x4F
+    jne .error
+
+    ; Success, fill vbe_screen structure
+    mov eax, [mode_info_block.framebuffer]
+    mov [vbe_screen.physical_buffer], eax
+
+    clc
+    ret
+
+.next_mode:
+    mov ax, [.segment]
+    mov fs, ax
+    mov si, [.offset]
+    jmp .find_mode
+
+.error:
+    stc
+    ret
+
+.width      dw 0
+.height     dw 0
+.bpp        db 0
+.segment    dw 0
+.offset     dw 0
+.mode       dw 0
 
 
 KbdControllerDataPort               equ 0x60
@@ -125,8 +231,6 @@ KbdControllerDisableKeyboard        equ 0xAD
 KbdControllerEnableKeyboard         equ 0xAE
 KbdControllerReadCtrlOutputPort     equ 0xD0
 KbdControllerWriteCtrlOutputPort    equ 0xD1
-
-ScreenBuffer                        equ 0xB8000
 
 g_GDT:      ; NULL descriptor
             dq 0
@@ -167,3 +271,60 @@ g_GDTDesc:  dw g_GDTDesc - g_GDT - 1    ; limit = size of GDT
             dd g_GDT                    ; address of GDT
 
 g_BootDrive: db 0
+
+section .bss
+
+; VBE Info Block
+vbe_info_block:
+    .signature          resb 4
+    .version            resw 1
+    .oem_string_ptr     resd 1
+    .capabilities       resd 1
+    .video_modes_ptr    resd 1
+    .total_memory       resw 1
+    .oem_software_rev   resw 1
+    .oem_vendor_name_ptr resd 1
+    .oem_product_name_ptr resd 1
+    .oem_product_rev_ptr resd 1
+    .reserved           resb 222
+    .oem_data           resb 256
+
+; VBE Mode Info Block
+mode_info_block:
+    .attributes         resw 1
+    .window_a           resb 1
+    .window_b           resb 1
+    .granularity        resw 1
+    .window_size        resw 1
+    .segment_a          resw 1
+    .segment_b          resw 1
+    .win_func_ptr       resd 1
+    .pitch              resw 1
+    .width              resw 1
+    .height             resw 1
+    .char_width         resb 1
+    .char_height        resb 1
+    .planes             resb 1
+    .bpp                resb 1
+    .banks              resb 1
+    .memory_model       resb 1
+    .bank_size          resb 1
+    .image_pages        resb 1
+    .reserved0          resb 1
+    .red_mask           resb 1
+    .red_position       resb 1
+    .green_mask         resb 1
+    .green_position     resb 1
+    .blue_mask          resb 1
+    .blue_position      resb 1
+    .reserved_mask      resb 1
+    .reserved_position  resb 1
+    .direct_color_attributes resb 1
+    .framebuffer        resd 1
+    .off_screen_mem_offset resd 1
+    .off_screen_mem_size resw 1
+    .reserved1          resb 206
+
+; This structure will hold the graphics mode info for the kernel
+vbe_screen:
+    .physical_buffer    resd 1
