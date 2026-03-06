388801e5794af7af26beb71b45c27a198c364222
MrMagoo8888 <amwilson218@gmail.com>
2026-03-06 23:21:24 +0000

    KERNEL!! With init errors

--- src/bootloader/stage2/main.c
+++ src/bootloader/stage2/main.c
@@ -7,11 +7,12 @@
 #include "memory.h"
 #include "vbe.h"
 #include "graphics.h"
+#include "stddef.h"
 
 uint8_t* KernelLoadBuffer = (uint8_t*)MEMORY_LOAD_KERNEL;
 uint8_t* Kernel = (uint8_t*)MEMORY_KERNEL_ADDR;
 
-typedef void (*KernelStart)();
+typedef void (*KernelStart)(VbeScreenInfo* vbe_info, uint16_t bootDrive);
 
 void __attribute__((cdecl)) start(uint16_t bootDrive)
 {
@@ -24,21 +25,28 @@ void __attribute__((cdecl)) start(uint16_t bootDrive)
     if (!DISK_Initialize(&disk, bootDrive))
     {
         printf("Disk init error\r\n");
-        goto end;
         draw_pixel(100, 800, 0x00FF0000); // Draw a RED pixel
+        goto end;
     }
     draw_pixel(200, 200, 0x00FF6699); // Draw a PINK pixel
 
     if (!FAT_Initialize(&disk))
     {
         printf("FAT init error\r\n");
-        goto end;
         draw_pixel(200, 800, 0x00123456); // Draw a deep sea blue pixel
+        goto end;
     }
     draw_pixel(250, 250, 0x00FFFF00); // Draw a YELLOW pixel
 
     // load kernel
     FAT_File* fd = FAT_Open(&disk, "/kernel.bin");
+    if (fd == NULL)
+    {
+        printf("Kernel not found\r\n");
+        draw_pixel(250, 800, 0x00FF0000); // Draw a RED pixel
+        goto end;
+    }
+
     uint32_t read;
     uint8_t* kernelBuffer = Kernel;
     while ((read = FAT_Read(&disk, fd, MEMORY_LOAD_SIZE, KernelLoadBuffer)))
@@ -61,15 +69,7 @@ void __attribute__((cdecl)) start(uint16_t bootDrive)
     printf("  Physical Buffer: 0x%x\r\n", vbe_screen.physical_buffer);
     draw_pixel(350, 350, 0x0000FFFF); // Draw a CYAN pixel
     KernelStart kernelStart = (KernelStart)Kernel; // Kernel's entry point
-    __asm__ volatile (
-        "push %0\n\t"
-        "push %1\n\t"
-        "call *%2\n\t"
-        "add $8, %%esp"
-        : /* no outputs */
-        : "r"((uint32_t)bootDrive), "r"(&vbe_screen), "r"(kernelStart)
-        : "memory"
-    );
+    kernelStart(&vbe_screen, bootDrive);
 
     draw_pixel(300, 800, 0x0000FFFF); // Draw a CYAN pixel
 end:
