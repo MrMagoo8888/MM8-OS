2c0ff496e9ff70f059920fff08ee12471ef3f8e5
MrMagoo8888 <mrmagoo8888@yahoo.com>
2026-08-13 20:47:35 +0100

    sure

--- build_scripts/toolchain.mk
+++ build_scripts/toolchain.mk
@@ -12,7 +12,7 @@ toolchain_binutils: $(TOOLCHAIN_PREFIX)/bin/i686-elf-ld
 $(TOOLCHAIN_PREFIX)/bin/i686-elf-ld: $(BINUTILS_SRC).tar.xz
 	cd toolchain && tar -xf binutils-$(BINUTILS_VERSION).tar.xz
 	mkdir $(BINUTILS_BUILD)
-	cd $(BINUTILS_BUILD) && CFLAGS= ASMFLAGS= CC= CXX= LD= ASM= LINKFLAGS= LIBS= ../binutils-$(BINUTILS_VERSION)/configure \
+	cd $(BINUTILS_BUILD) && CFLAGS="-Wno-error -fpermissive" CXXFLAGS="-Wno-error -fpermissive" ASMFLAGS= CC= CXX= LD= ASM= LINKFLAGS= LIBS= ../binutils-$(BINUTILS_VERSION)/configure \
 		--prefix="$(TOOLCHAIN_PREFIX)"	\
 		--target=$(TARGET)				\
 		--with-sysroot					\
@@ -20,6 +20,7 @@ $(TOOLCHAIN_PREFIX)/bin/i686-elf-ld: $(BINUTILS_SRC).tar.xz
 		--disable-werror
 	$(MAKE) -j8 -C $(BINUTILS_BUILD)
 	$(MAKE) -C $(BINUTILS_BUILD) install
+
 
 $(BINUTILS_SRC).tar.xz:
 	mkdir -p toolchain 
@@ -33,8 +34,9 @@ toolchain_gcc: $(TOOLCHAIN_PREFIX)/bin/i686-elf-gcc
 
 $(TOOLCHAIN_PREFIX)/bin/i686-elf-gcc: $(TOOLCHAIN_PREFIX)/bin/i686-elf-ld $(GCC_SRC).tar.xz
 	cd toolchain && tar -xf gcc-$(GCC_VERSION).tar.xz
+	cd $(GCC_SRC) && ./contrib/download_prerequisites
 	mkdir $(GCC_BUILD)
-	cd $(GCC_BUILD) && CFLAGS= ASMFLAGS= CC= CXX= LD= ASM= LINKFLAGS= LIBS= ../gcc-$(GCC_VERSION)/configure \
+	cd $(GCC_BUILD) && CFLAGS="-Wno-error -fpermissive" CXXFLAGS="-Wno-error -fpermissive" ASMFLAGS= CC= CXX= LD= ASM= LINKFLAGS= LIBS= ../gcc-$(GCC_VERSION)/configure \
 		--prefix="$(TOOLCHAIN_PREFIX)" 	\
 		--target=$(TARGET)				\
 		--disable-nls					\
@@ -42,6 +44,8 @@ $(TOOLCHAIN_PREFIX)/bin/i686-elf-gcc: $(TOOLCHAIN_PREFIX)/bin/i686-elf-ld $(GCC_
 		--without-headers
 	$(MAKE) -j8 -C $(GCC_BUILD) all-gcc all-target-libgcc
 	$(MAKE) -C $(GCC_BUILD) install-gcc install-target-libgcc
+
+
 	
 $(GCC_SRC).tar.xz:
 	mkdir -p toolchain
